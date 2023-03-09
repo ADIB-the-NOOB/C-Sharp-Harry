7757a4f1f9ae2879e97c5f517d79955f3965ef7f
ADIB-the-NOOB <mdadib550@gmail.com>
2023-03-09 21:39:46 +0600

    Add project files.

---
+++ LearnFromHarry/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNumber = 23;
+            Console.WriteLine(myNumber);
+            double myDouble = 23.2D;
+            Console.WriteLine(myDouble);
+
+            // this is a Boolean
+            bool myBool = false;
+            Console.WriteLine(myBool);
+
+            //
+        }
+    }   
+}
